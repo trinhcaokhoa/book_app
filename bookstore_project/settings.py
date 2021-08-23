d0ae0657c4e072c0d0016a796dfcc743c19b9f3b
Khoa Trinh <caokhoatrinh@gmail.com>
2021-08-23 14:43:55 -0700

    UPGRADE - upgrade debug and secretkey to env

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k@h8a%#cga!sshc^0(ws8!njt!9-f=w8$@isjoy@kz-h=do%+j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
